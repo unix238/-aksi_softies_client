28216b2d7d4b63600c28a930782a33bedbd91fb6
unix238 <bekzot.abdyldayev@gmail.com>
2023-02-14 20:15:26 +0600

    sorts minor fix

--- src/localizations/SortsAndFilters.js
+++ src/localizations/SortsAndFilters.js
@@ -1,140 +1,143 @@
 export const SortsAndFilters = [
-    [
-        {
-            title: 'Cортировать по',
-            options: [
-                { text: 'По пулярности', value: 'popularity' },
-                { text: 'Убыванию цены', value: 'price-desc' },
-                { text: 'Возрастанию цены', value: 'price-asc' },
-                { text: 'Новизне', value: 'new' },
-            ],
-        },
-        {
-            title: 'Размер',
-            options: [
-                { text: '5-10см', value: '5-10' },
-                { text: '10-15см', value: '10-15' },
-                { text: '15-20см', value: '15-20' },
-                { text: '20-25см', value: '20-25' },
-                { text: '25-30см', value: '25-30' },
-            ],
-        },
-        {
-            title: 'Цена',
-            options: [
-                { text: '0 - 5 000₸', value: '0-5000' },
-                { text: '5 000 - 10 000₸', value: '5000-10000' },
-                { text: '10 000 - 15 000₸', value: '10000-15000' },
-                { text: '20 000 - 25 000₸', value: '20000-25000' },
-                { text: '25 000 - 30 000₸', value: '25000-30000' },
-            ],
-        },
-        {
-            title: 'Пряжа',
-            options: [
-                { text: 'Полиэстер', value: 'cotton' },
-                { text: 'Акрил', value: 'acrylic' },
-            ],
-        },
-        {
-            title: 'Категория',
-            options: [
-                { text: 'Все', value: 'all' },
-                { text: 'Игрушки', value: 'toys' },
-                { text: 'Схемы', value: 'schemes' },
-            ],
-        },
-    ],
-    [
-        {
-            title: 'Cортировать по',
-            options: [
-                { text: 'По пулярности', value: 'popularity' },
-                { text: 'Убыванию цены', value: 'price-desc' },
-                { text: 'Возрастанию цены', value: 'price-asc' },
-                { text: 'Новизне', value: 'new' },
-            ],
-        },
-        {
-            title: 'Размер',
-            options: [
-                { text: '5-10см', value: '5-10' },
-                { text: '10-15см', value: '10-15' },
-                { text: '15-20см', value: '15-20' },
-                { text: '20-25см', value: '20-25' },
-                { text: '25-30см', value: '25-30' },
-            ],
-        },
-        {
-            title: 'Цена',
-            options: [
-                { text: '0 - 5 000₸', value: '0-5000' },
-                { text: '5 000 - 10 000₸', value: '5000-10000' },
-                { text: '10 000 - 15 000₸', value: '10000-15000' },
-                { text: '20 000 - 25 000₸', value: '20000-25000' },
-                { text: '25 000 - 30 000₸', value: '25000-30000' },
-            ],
-        },
-        {
-            title: 'Пряжа',
-            options: [
-                { text: 'Полиэстер', value: 'cotton' },
-                { text: 'Акрил', value: 'acrylic' },
-            ],
-        },
-        {
-            title: 'Категория',
-            options: [
-                { text: 'Все', value: 'all' },
-                { text: 'Игрушки', value: 'toys' },
-                { text: 'Схемы', value: 'schemes' },
-            ],
-        },
-    ],
-    [
-        {
-            title: 'Cортировать по',
-            options: [
-                { text: 'По пулярности', value: 'popularity' },
-                { text: 'Убыванию цены', value: 'price-desc' },
-                { text: 'Возрастанию цены', value: 'price-asc' },
-                { text: 'Новизне', value: 'new' },
-            ],
-        },
-        {
-            title: 'Размер',
-            options: [
-                { text: '5-10см', value: '5-10' },
-                { text: '10-15см', value: '10-15' },
-                { text: '15-20см', value: '15-20' },
-                { text: '20-25см', value: '20-25' },
-                { text: '25-30см', value: '25-30' },
-            ],
-        },
-        {
-            title: 'Цена',
-            options: [
-                { text: '0 - 5 000₸', value: '0-5000' },
-                { text: '5 000 - 10 000₸', value: '5000-10000' },
-                { text: '10 000 - 15 000₸', value: '10000-15000' },
-                { text: '20 000 - 25 000₸', value: '20000-25000' },
-                { text: '25 000 - 30 000₸', value: '25000-30000' },
-            ],
-        },
-        {
-            title: 'Пряжа',
-            options: [
-                { text: 'Полиэстер', value: 'cotton' },
-                { text: 'Акрил', value: 'acrylic' },
-            ],
-        },
-        {
-            title: 'Категория',
-            options: [
-                { text: 'Все', value: 'all' },
-                { text: 'Игрушки', value: 'toys' },
-                { text: 'Схемы', value: 'schemes' },
-            ],
-        },
-    ],
+  [
+    {
+      title: 'Cортировать по',
+      options: [
+        { text: 'По пулярности', value: 'popularity' },
+        { text: 'Убыванию цены', value: 'price-desc' },
+        { text: 'Возрастанию цены', value: 'price-asc' },
+        { text: 'Новизне', value: 'new' },
+      ],
+    },
+    {
+      title: 'Размер',
+      options: [
+        { text: '5-10см', value: '5-10' },
+        { text: '10-15см', value: '10-15' },
+        { text: '15-20см', value: '15-20' },
+        { text: '20-25см', value: '20-25' },
+        { text: '25-30см', value: '25-30' },
+      ],
+    },
+    {
+      title: 'Цена',
+      options: [
+        { text: '0 - 5 000₸', value: '0-5000' },
+        { text: '5 000 - 10 000₸', value: '5000-10000' },
+        { text: '10 000 - 15 000₸', value: '10000-15000' },
+        { text: '20 000 - 25 000₸', value: '20000-25000' },
+        { text: '25 000 - 30 000₸', value: '25000-30000' },
+      ],
+    },
+    {
+      title: 'Пряжа',
+      options: [
+        { text: 'Полиэстер', value: '63e92cca9dc24cb8dde6ca3b' },
+        { text: 'Акрил', value: '63e92cdb9dc24cb8dde6ca3d' },
+        { text: 'Хлопок', value: '63e92cfc9dc24cb8dde6ca3f' },
+      ],
+    },
+    {
+      title: 'Категория',
+      options: [
+        { text: 'Все', value: 'all' },
+        { text: 'Игрушки', value: 'toys' },
+        { text: 'Схемы', value: 'schemes' },
+      ],
+    },
+  ],
+  [
+    {
+      title: 'Cортировать по',
+      options: [
+        { text: 'По пулярности', value: 'popularity' },
+        { text: 'Убыванию цены', value: 'price-desc' },
+        { text: 'Возрастанию цены', value: 'price-asc' },
+        { text: 'Новизне', value: 'new' },
+      ],
+    },
+    {
+      title: 'Размер',
+      options: [
+        { text: '5-10см', value: '5-10' },
+        { text: '10-15см', value: '10-15' },
+        { text: '15-20см', value: '15-20' },
+        { text: '20-25см', value: '20-25' },
+        { text: '25-30см', value: '25-30' },
+      ],
+    },
+    {
+      title: 'Цена',
+      options: [
+        { text: '0 - 5 000₸', value: '0-5000' },
+        { text: '5 000 - 10 000₸', value: '5000-10000' },
+        { text: '10 000 - 15 000₸', value: '10000-15000' },
+        { text: '20 000 - 25 000₸', value: '20000-25000' },
+        { text: '25 000 - 30 000₸', value: '25000-30000' },
+      ],
+    },
+    {
+      title: 'Пряжа',
+      options: [
+        { text: 'Полиэстер', value: '63e92cca9dc24cb8dde6ca3b' },
+        { text: 'Акрил', value: '63e92cdb9dc24cb8dde6ca3d' },
+        { text: 'Хлопок', value: '63e92cfc9dc24cb8dde6ca3f' },
+      ],
+    },
+    {
+      title: 'Категория',
+      options: [
+        { text: 'Все', value: 'all' },
+        { text: 'Игрушки', value: 'toys' },
+        { text: 'Схемы', value: 'schemes' },
+      ],
+    },
+  ],
+  [
+    {
+      title: 'Cортировать по',
+      options: [
+        { text: 'По пулярности', value: 'popularity' },
+        { text: 'Убыванию цены', value: 'price-desc' },
+        { text: 'Возрастанию цены', value: 'price-asc' },
+        { text: 'Новизне', value: 'new' },
+      ],
+    },
+    {
+      title: 'Размер',
+      options: [
+        { text: '5-10см', value: '5-10' },
+        { text: '10-15см', value: '10-15' },
+        { text: '15-20см', value: '15-20' },
+        { text: '20-25см', value: '20-25' },
+        { text: '25-30см', value: '25-30' },
+      ],
+    },
+    {
+      title: 'Цена',
+      options: [
+        { text: '0 - 5 000₸', value: '0-5000' },
+        { text: '5 000 - 10 000₸', value: '5000-10000' },
+        { text: '10 000 - 15 000₸', value: '10000-15000' },
+        { text: '20 000 - 25 000₸', value: '20000-25000' },
+        { text: '25 000 - 30 000₸', value: '25000-30000' },
+      ],
+    },
+    {
+      title: 'Пряжа',
+      options: [
+        { text: 'Полиэстер', value: '63e92cca9dc24cb8dde6ca3b' },
+        { text: 'Акрил', value: '63e92cdb9dc24cb8dde6ca3d' },
+        { text: 'Хлопок', value: '63e92cfc9dc24cb8dde6ca3f' },
+      ],
+    },
+    {
+      title: 'Категория',
+      options: [
+        { text: 'Все', value: 'all' },
+        { text: 'Игрушки', value: 'toys' },
+        { text: 'Схемы', value: 'schemes' },
+      ],
+    },
+  ],
 ];
