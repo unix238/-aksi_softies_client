5f521985c85fa1b056ab33d8428abda5c7ef69ae
Beka <bekzot.abdyldayev@gmailcom>
2023-02-15 10:19:27 +0600

    fixed images in detail, itemcard and basketcard

--- src/components/BasketItemCard/BasketItemCard.jsx
+++ src/components/BasketItemCard/BasketItemCard.jsx
@@ -5,74 +5,65 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchItems, getItemById } from '../../actions/itemsActions';
 import { Link } from 'react-router-dom';
 import { removeItemFromBasket } from '../../actions/basketActions';
+import config from '../../config/config';
 
 export const BasketItemCard = ({ id }) => {
-    const currentLanguage = useSelector(
-        (state) => state.global.currentLanguage
-    );
-    const [currentItem, setCurrentItem] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+  const currentLanguage = useSelector((state) => state.global.currentLanguage);
+  const [currentItem, setCurrentItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        getItemById(id.item._id, setCurrentItem, setIsLoading);
-    }, []);
+  useEffect(() => {
+    getItemById(id.item._id, setCurrentItem, setIsLoading);
+  }, []);
 
-    const dispatch = useDispatch();
-    const removeItemFromBasketHandler = () => {
-        dispatch(removeItemFromBasket(id.item._id));
-        console.log('pressed');
-    };
+  const dispatch = useDispatch();
+  const removeItemFromBasketHandler = () => {
+    dispatch(removeItemFromBasket(id.item._id));
+    console.log('pressed');
+  };
 
-    return isLoading ? (
-        <></>
-    ) : (
-        <>
-            <div className={cl.item}>
-                <div className={cl.left}>
-                    <Link to={`/detail/${currentItem._id}`}>
-                        <div className={cl.image}>
-                            <img src={currentItem.images[0]} />
-                        </div>
-                    </Link>
-                    <div className={cl.texts}>
-                        <div className={cl.title}>
-                            Игрушка{' '}
-                            <span className={cl.bold}>
-                                {currentItem.title[currentLanguage]}
-                            </span>
-                        </div>
-                        <div className={cl.size}>
-                            Размер: {currentItem.sizes[id.size]}
-                        </div>
-                    </div>
-                </div>
-                <div className={cl.mid}>
-                    <div className={cl.quantity}>
-                        <span className={cl.bold}>{id.quantity}шт.</span>
-                    </div>
-                </div>
-                <div className={cl.price}>
-                    <div className={cl.price__text}>
-                        <span className={cl.bold}>
-                            {currentItem.price[id.size].toLocaleString(
-                                undefined,
-                                {
-                                    maximumFractionDigits: 2,
-                                }
-                            )}{' '}
-                            ₸
-                        </span>
-                    </div>
-                    <div
-                        className={cl.button}
-                        onClick={removeItemFromBasketHandler}
-                    >
-                        Удалить
-                    </div>
-                </div>
+  return isLoading ? (
+    <></>
+  ) : (
+    <>
+      <div className={cl.item}>
+        <div className={cl.left}>
+          <Link to={`/detail/${currentItem._id}`}>
+            <div className={cl.image}>
+              <img src={config.url + currentItem.images[0]} />
             </div>
+          </Link>
+          <div className={cl.texts}>
+            <div className={cl.title}>
+              Игрушка{' '}
+              <span className={cl.bold}>
+                {currentItem.title[currentLanguage]}
+              </span>
+            </div>
+            <div className={cl.size}>Размер: {currentItem.sizes[id.size]}</div>
+          </div>
+        </div>
+        <div className={cl.mid}>
+          <div className={cl.quantity}>
+            <span className={cl.bold}>{id.quantity}шт.</span>
+          </div>
+        </div>
+        <div className={cl.price}>
+          <div className={cl.price__text}>
+            <span className={cl.bold}>
+              {currentItem.price[id.size].toLocaleString(undefined, {
+                maximumFractionDigits: 2,
+              })}{' '}
+              ₸
+            </span>
+          </div>
+          <div className={cl.button} onClick={removeItemFromBasketHandler}>
+            Удалить
+          </div>
+        </div>
+      </div>
 
-            <div className={cl.line}></div>
-        </>
-    );
+      <div className={cl.line}></div>
+    </>
+  );
 };
--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -5,42 +5,37 @@ import { Cart } from '../Icons/Cart';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItemToBasket } from '../../actions/basketActions';
 import { Link } from 'react-router-dom';
+import config from '../../config/config';
 
 export const ItemCard = ({ item }) => {
-    const price = item.price[0];
-    const currentLanguage = useSelector(
-        (state) => state.global.currentLanguage
-    );
-    const dispatch = useDispatch();
-    return (
-        <div className={cl.item}>
-            <Link
-                className='item__link'
-                to={`/detail/${item._id}`}
-                key={item._id}
-            >
-                <div className={cl.image}>
-                    {/* <img src={image} alt='' /> */}
-                    <img src={item.images[0]} alt='' />
-                </div>
-            </Link>
-            <div className={cl.title}>{item.title[currentLanguage]}</div>
-            <div className={cl.line}>
-                <div className={cl.price}>
-                    {price.toLocaleString(undefined, {
-                        maximumFractionDigits: 2,
-                    })}{' '}
-                    ₸
-                </div>
-                <div
-                    className={cl.icon}
-                    onClick={() => {
-                        dispatch(addItemToBasket(item, 1, 0));
-                    }}
-                >
-                    <Cart />
-                </div>
-            </div>
+  const price = item.price[0];
+  const currentLanguage = useSelector((state) => state.global.currentLanguage);
+  const dispatch = useDispatch();
+  return (
+    <div className={cl.item}>
+      <Link className='item__link' to={`/detail/${item._id}`} key={item._id}>
+        <div className={cl.image}>
+          {/* <img src={image} alt='' /> */}
+          <img src={config.url + item.images[0]} alt='' />
         </div>
-    );
+      </Link>
+      <div className={cl.title}>{item.title[currentLanguage]}</div>
+      <div className={cl.line}>
+        <div className={cl.price}>
+          {price.toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+          })}{' '}
+          ₸
+        </div>
+        <div
+          className={cl.icon}
+          onClick={() => {
+            dispatch(addItemToBasket(item, 1, 0));
+          }}
+        >
+          <Cart />
+        </div>
+      </div>
+    </div>
+  );
 };
